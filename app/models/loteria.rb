09b7e8e230f8740f958e04223ac7ca125e42d414
rmorali <rodrigo.morali@gmail.com>
2015-05-11 08:26:03 -0700

    Transforma campos em arrays

--- app/models/loteria.rb
+++ app/models/loteria.rb
@@ -1,8 +1,8 @@
 class Loteria < ActiveRecord::Base
-  serialize  :linhas,
-             :colunas,
-             :colunas_da_esquerda,
-             :colunas_da_direita,
-             :menos_sorteados,
-             :multiplos
+  serialize :linhas
+  serialize :colunas
+  serialize :colunas_da_esquerda
+  serialize :colunas_da_direita
+  serialize :menos_sorteados
+  serialize :multiplos
 end
