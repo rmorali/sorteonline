c631095addb6a76f79216166f5e5b2e13e278047
rmorali <rodrigo.morali@gmail.com>
2015-05-22 13:58:41 -0700

    Teste leitura de arquivo

--- spec/factories.rb
+++ spec/factories.rb
@@ -7,7 +7,7 @@ FactoryGirl.define do
   end
 
   factory :bolao do
-    arquivo_file_name 'QN-Teste'
+    arquivo_file_name 'QN-Teste.txt'
   end
 
 end
--- spec/models/bolao_spec.rb
+++ spec/models/bolao_spec.rb
@@ -4,5 +4,11 @@ describe Bolao do
 
   it { should have_attached_file(:arquivo) }
   it { should validate_attachment_presence(:arquivo) }
+  it { have_many :palpites }
+
+  it 'should have archive contents' do
+    bolao = build(:bolao)
+    bolao.arquivo_contents.should_not == 'vazio'
+  end
 
 end
